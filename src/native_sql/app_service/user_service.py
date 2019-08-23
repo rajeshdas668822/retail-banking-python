047a7a456cd450b3cdb169600e6d186ba32a5b3c
rdas <rdas@numerix.com>
2019-08-23 06:05:46 -0400

    Fixed the blue print issue. Still need to do some work on marshmallow though

---
+++ src/native_sql/app_service/user_service.py
@@ -0,0 +1,41 @@
+from native_sql.data_access.data_setup import dal
+from sqlalchemy import select, update, delete
+from native_sql.app_service.base_service import BaseService
+from util.result_helper import ResultHelper
+
+
+class UserService(BaseService):
+
+    def __init__(self, default_dao=None):
+        self.default_dao = default_dao
+        # dal.db_init(conn_string)
+
+    def save(self, data):
+        users = [data.__dict__]
+        ins = dal.users.insert()
+        self.save_data(ins, users)
+        select_query = select([dal.users]).where(dal.users.columns.login_id == data.login_id)
+        result_proxy = self.default_dao.find_by_query(select_query)
+        return ResultHelper.resultproxy_to_dict_list(result_proxy)
+
+    def get_all(self):
+        select_all_users = select([dal.users])
+        results = self.default_dao.find_by_query(select_all_users)
+        return ResultHelper.resultproxy_to_dict_list(results)
+
+    def update(self, data):
+        users = [data.__dict__]
+        print("user_id ======{}".format(data.user_id))
+        updated_user = update(dal.users).where(dal.users.columns.user_id == data.user_id)
+        self.default_dao.update_data(updated_user, users)
+        select_query = select([dal.users]).where(dal.users.columns.user_id == data.user_id)
+        results = self.default_dao.find_by_query(select_query)
+        return ResultHelper.resultproxy_to_dict_list(results)
+
+    def delete_by_id(self, user_id):
+        delete_user = delete(dal.users).where(dal.users.columns.user_id == user_id)
+        result = self.default_dao.delete_data(delete_user)
+        print("result.rowcount---> {}".format(result.rowcount))
+
+
+
---
+++ src/native_sql/model/Account.py
@@ -0,0 +1,33 @@
+from util.CustomException import InsufficientBalance
+
+
+class Account:
+    def __init__(self, account_number, name, account_type,balance, customer_ref, user_id):
+        self.name = name
+        self.account_number = account_number
+        self.balance = balance
+        self.account_type = account_type
+        self.customer_ref = customer_ref
+        self.created_by = user_id
+        self.updated_by = user_id
+
+    def get_name(self):
+        return self.name
+
+    def get_account_number(self):
+        return self.account_number
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientBalance("Balance is not enough")
+        else:
+            self.balance -= amount
+
+    def get_balance(self):
+        return self.balance
+
+    def set_balance(self, balance):
+        self.balance = balance
