7a9403831db1ebfe521257735584321d431919df
rdas <rdas@numerix.com>
2019-08-29 05:15:50 -0400

    Added more info in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,116 @@
 import pytest
 from app_config.config import conn_string
 from orm.orm_data_access.orm_setup import OrmHelper
+from sqlalchemy_utils import create_database, drop_database, database_exists
+from orm.orm_data_access.models import Base
+
+
+SQLITE_TEST_DB_URI = 'SQLITE_TEST_DB_URI'
+MYSQL_TEST_DB_URI = 'MYSQL_TEST_DB_URI'
+POSTGRESQL_TEST_DB_URI = 'POSTGRESQL_TEST_DB_URI'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--sqlite-test-db-uri',
+        action='store',
+        dest=SQLITE_TEST_DB_URI,
+        default='sqlite:///test_sqlalchemy_filters.db',
+        help=(
+            'DB uri for testing (e.g. '
+            '"sqlite:///test_sqlalchemy_filters.db")'
+        )
+    )
+
+@pytest.fixture(scope='session')
+def config(request):
+    # return {
+    #     SQLITE_TEST_DB_URI: request.config.getoption(SQLITE_TEST_DB_URI),
+    #     MYSQL_TEST_DB_URI: request.config.getoption(MYSQL_TEST_DB_URI),
+    #     POSTGRESQL_TEST_DB_URI: request.config.getoption(POSTGRESQL_TEST_DB_URI),
+    # }
+    return { SQLITE_TEST_DB_URI : "sqlite:///test_sqlalchemy_filters.db"
+    }
+
+
+def test_db_keys():
+    """Decide what DB backends to use to run the tests."""
+    test_db_uris = []
+    test_db_uris.append(SQLITE_TEST_DB_URI)
+
+    try:
+        import mysql  # noqa: F401
+    except ImportError:
+        pass
+    else:
+        test_db_uris.append(MYSQL_TEST_DB_URI)
+
+    try:
+        import psycopg2  # noqa: F401
+    except ImportError:
+        pass
+    else:
+        test_db_uris.append(POSTGRESQL_TEST_DB_URI)
+
+    return test_db_uris
+
+
+@pytest.fixture(scope='session', params=test_db_keys())
+def db_uri(request, config):
+    return config[request.param]
+
+
+@pytest.fixture(scope='session')
+def is_postgresql(db_uri):
+    if 'postgresql' in db_uri:
+        return True
+    return False
+
+
+@pytest.fixture(scope='session')
+def is_sqlite(db_uri):
+    if 'sqlite' in db_uri:
+        return True
+    return False
+
+
+@pytest.fixture(scope='session')
+def db_engine_options(db_uri, is_postgresql):
+    if is_postgresql:
+        return dict(
+            client_encoding='utf8',
+            connect_args={'client_encoding': 'utf8'}
+        )
+    return {}
+
 
 
 @pytest.fixture()
-def session():
+def session(db_uri, db_engine_options):
     orm_helper = OrmHelper()
     orm_helper.db_init(conn_string)
     db_session = orm_helper.get_session()
-    print(" Session Called ..............")
+    print("Session Called ..............")
     yield db_session
+
+    for table in reversed(Base.metadata.sorted_tables):
+        db_session.execute(table.delete())
+
     db_session.commit()
     db_session.close()
+
+
+
+def create_db(uri):
+    """Drop the database at ``uri`` and create a brand new one. """
+    destroy_database(uri)
+    create_database(uri)
+
+
+def destroy_database(uri):
+    """Destroy the database at ``uri``, if it exists. """
+    if database_exists(uri):
+        drop_database(uri)
+
 
 
--- tests/filter/test_filters.py
+++ tests/filter/test_filters.py
@@ -58,7 +58,7 @@ class TestFilterNotApplied:
         assert expected_error == err.value.args[0]
 
 
-    #@pytest.mark.usefixtures('multiple_users_inserted')
+    @pytest.mark.usefixtures('multiple_users_inserted')
     def test_no_operator_provided(self, session):
         query = session.query(User)
         filters = [{'field': 'login_id', 'value': 'rdas'}]
@@ -100,7 +100,7 @@ class TestFilterNotApplied:
             apply_filters(query, filters)
 
         expected_error = (
-            "Model <class 'test.models.Bar'> has no column `invalid_field`."
+            "Model <class 'orm.orm_data_access.models.User'> has no column `invalid_field`."
         )
         assert expected_error == err.value.args[0]
 
@@ -116,8 +116,129 @@ class TestFilterNotApplied:
             apply_filters(query, filters)
 
         expected_error = (
-            "Model <class 'test.models.Bar'> has no column `{}`.".format(
+            "Model <class 'orm.orm_data_access.models.User'> has no column `{}`.".format(
                 attr_name
             )
         )
         assert expected_error == err.value.args[0]
+
+
+# class TestMultipleModels:
+#
+#     # TODO: multi-model should be tested for each filter type
+#     @pytest.mark.usefixtures('multiple_bars_inserted')
+#     @pytest.mark.usefixtures('multiple_quxs_inserted')
+#     def test_multiple_models(self, session):
+#         query = session.query(Bar, Qux)
+#         filters = [
+#             {'model': 'Bar', 'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Qux', 'field': 'name', 'op': '==', 'value': 'name_1'},
+#         ]
+#
+#         filtered_query = apply_filters(query, filters)
+#         result = filtered_query.all()
+#
+#         assert len(result) == 4
+#         bars, quxs = zip(*result)
+#         assert set(map(type, bars)) == {Bar}
+#         assert {bar.id for bar in bars} == {1, 3}
+#         assert {bar.name for bar in bars} == {"name_1"}
+#         assert set(map(type, quxs)) == {Qux}
+#         assert {qux.id for qux in quxs} == {1, 3}
+#         assert {qux.name for qux in quxs} == {"name_1"}
+#
+#
+# class TestAutoJoin:
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_auto_join(self, session):
+#
+#         query = session.query(Foo)
+#         filters = [
+#             {'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#         ]
+#
+#         filtered_query = apply_filters(query, filters)
+#         result = filtered_query.all()
+#
+#         assert len(result) == 1
+#         assert result[0].id == 3
+#         assert result[0].bar_id == 3
+#         assert result[0].bar.count is None
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_do_not_auto_join(self, session):
+#
+#         query = session.query(Foo)
+#         filters = [
+#             {'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#         ]
+#
+#         with pytest.raises(BadSpec) as exc:
+#             apply_filters(query, filters, do_auto_join=False)
+#
+#         assert 'The query does not contain model `Bar`' in str(exc)
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_noop_if_query_contains_named_models(self, session):
+#
+#         query = session.query(Foo).join(Bar)
+#         filters = [
+#             {'model': 'Foo', 'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#         ]
+#
+#         filtered_query = apply_filters(query, filters)
+#         result = filtered_query.all()
+#
+#         assert len(result) == 1
+#         assert result[0].id == 3
+#         assert result[0].bar_id == 3
+#         assert result[0].bar.count is None
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_auto_join_to_invalid_model(self, session):
+#
+#         query = session.query(Foo)
+#         filters = [
+#             {'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#             {'model': 'Qux', 'field': 'created_at', 'op': 'is_not_null'}
+#         ]
+#         with pytest.raises(BadSpec) as err:
+#             apply_filters(query, filters)
+#
+#         assert 'The query does not contain model `Qux`.' == err.value.args[0]
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_ambiguous_query(self, session):
+#
+#         query = session.query(Foo).join(Bar)
+#         filters = [
+#             {'field': 'name', 'op': '==', 'value': 'name_1'},  # ambiguous
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#         ]
+#         with pytest.raises(BadSpec) as err:
+#             apply_filters(query, filters)
+#
+#         assert 'Ambiguous spec. Please specify a model.' == err.value.args[0]
+#
+#     @pytest.mark.usefixtures('multiple_foos_inserted')
+#     def test_eager_load(self, session):
+#
+#         # behaves as if the joinedload wasn't present
+#         query = session.query(Foo).options(joinedload(Foo.bar))
+#         filters = [
+#             {'field': 'name', 'op': '==', 'value': 'name_1'},
+#             {'model': 'Bar', 'field': 'count', 'op': 'is_null'},
+#         ]
+#
+#         filtered_query = apply_filters(query, filters)
+#         result = filtered_query.all()
+#
+#         assert len(result) == 1
+#         assert result[0].id == 3
+#         assert result[0].bar_id == 3
+#         assert result[0].bar.count is None
